1829f3093c12afe750df0b50a65b7910e18169f4
Allen Kang <allenk@microsoft.com>
2019-04-04 15:45:34 +0800

    init

--- kubernetes/voter-api/aks-deploy.sh
+++ kubernetes/voter-api/aks-deploy.sh
@@ -12,3 +12,18 @@ kubectl apply \
 	-f ./secrets/azure-cosmosdb-voter-secret.yaml \
 	-f ./secrets/azure-service-bus-secret.yaml \
 # 	-f ./secrets/tls-api-voter-demo-secret.yaml
+
+# pods
+kubectl apply \
+  -f ./services/election-deployment.yaml \
+  -f ./services/candidate-deployment.yaml \
+  -f ./services/voter-deployment.yaml
+
+# services
+kubectl apply \
+  -f ./services/election-service.yaml \
+  -f ./services/candidate-service.yaml \
+  -f ./services/voter-service.yaml
+
+# ingress
+kubectl apply -f ./other/ingress.yaml
