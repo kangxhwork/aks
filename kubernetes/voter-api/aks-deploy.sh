33ace9eb4f5a7b987720d8db533aebd328bad517
Allen Kang <allenk@microsoft.com>
2019-04-04 15:53:12 +0800

    init

--- kubernetes/voter-api/aks-deploy.sh
+++ kubernetes/voter-api/aks-deploy.sh
@@ -5,25 +5,11 @@
 # namespace
 kubectl apply -f ./other/namespace.yaml
 
-# secrets
-kubectl apply \
-	-f ./secrets/azure-cosmosdb-election-secret.yaml \
-	-f ./secrets/azure-cosmosdb-candidate-secret.yaml \
-	-f ./secrets/azure-cosmosdb-voter-secret.yaml \
-	-f ./secrets/azure-service-bus-secret.yaml \
-# 	-f ./secrets/tls-api-voter-demo-secret.yaml
+# voter
+kubectl apply -f ./secrets/azure-cosmosdb-voter-secret.yaml
+kubectl apply -f ./services/voter-deployment.yaml
+kubectl apply -f ./services/voter-service.yaml
 
-# pods
-kubectl apply \
-  -f ./services/election-deployment.yaml \
-  -f ./services/candidate-deployment.yaml \
-  -f ./services/voter-deployment.yaml
-
-# services
-kubectl apply \
-  -f ./services/election-service.yaml \
-  -f ./services/candidate-service.yaml \
-  -f ./services/voter-service.yaml
 
 # ingress
-kubectl apply -f ./other/ingress.yaml
+# kubectl apply -f ./other/ingress.yaml
