e16d83ff45fdd2503d7b9971aaf27b2fc22100ec
Allen Kang <allenk@microsoft.com>
2019-04-07 11:23:38 +0800

    4-7

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,4 +1,12 @@
 #!/bin/bash, dX2iSKg0ynjFQ
+# start service on deployment server:
+sudo docker run -u root --rm -d -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
+
+
+### check kubernetes cluster
+kubectl get node -o wide
+kubectl get pod -n voting -o wide
+kubectl get service -n voting -o wide
 
 ################## install osba https://docs.microsoft.com/en-us/azure/aks/integrate-azure  #########################
 
